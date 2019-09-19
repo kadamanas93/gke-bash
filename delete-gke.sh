a33413c1d80cdcac0650475efc82452a73677ae2
Anas Kadam <Anas_Kadam@ultimatesoftware.com>
2019-09-19 03:06:14 -0400

    Add delete gke code

--- delete-gke.sh
+++ delete-gke.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 [ -z "${GOOGLE_CREDENTIALS}" ]  && echo >&2 "Need to set GOOGLE_CREDENTIALS"  && exit 1
 [ -z "${GCP_PROJECT}" ]         && echo >&2 "Need to set GCP_PROJECT"         && exit 1
 [ -z "${GCP_ZONE}" ]            && echo >&2 "Need to set GCP_ZONE"            && exit 1
@@ -11,3 +12,8 @@ for i in $CMDS
 do
 	type -P "$i" &>/dev/null && continue  || { echo "$i command not found."; exit 1; }
 done
+
+printf "${GREEN}Deleting GKE cluster...\n${NC}"
+gcloud auth activate-service-account --key-file "${GOOGLE_CREDENTIALS}"
+gcloud config set compute/zone "${GCP_ZONE}"
+gcloud container clusters delete "${GKE_NAME}" --quiet
